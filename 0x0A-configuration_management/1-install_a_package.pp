0b8db45f7bc8a2a01d4257542d1fd863d94fe291
Ngobidi <chibuzormaxwell@mail.com>
2024-01-19 04:32:31 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#!/usr/bin/pup
-# install flask from pip3 Version 2.1.0 from puppet
+#Install puppet-flask
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'gem',
 }
